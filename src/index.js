447c0aef4084c26ad22439ef3fc49c57aa75c265
cbarne02 <Christopher.Barnett@tufts.edu>
2019-08-06 11:25:25 -0400

    updated DAG to toggle potential edges, add directional arrows.

--- src/index.js
+++ src/index.js
@@ -18,109 +18,162 @@ function drawDAG() {
         ],
         "edges": [
             {"source": 0, "target": 1, "strip": false, "approx": false},
-            {"source": 0, "target": 2, "strip": false, "approx": true},
-            {"source": 0, "target": 3, "strip": false, "approx": true},
-            {"source": 0, "target": 4, "strip": false, "approx": true},
-            {"source": 0, "target": 5, "strip": false, "approx": true},
-            {"source": 0, "target": 6, "strip": false, "approx": true},
-            {"source": 0, "target": 7, "strip": false, "approx": true},
-            {"source": 0, "target": 8, "strip": false, "approx": true},
             {"source": 1, "target": 2, "strip": false, "approx": false},
-            {"source": 1, "target": 3, "strip": false, "approx": true},
-            {"source": 1, "target": 4, "strip": false, "approx": true},
-            {"source": 1, "target": 5, "strip": false, "approx": true},
-            {"source": 1, "target": 6, "strip": false, "approx": true},
-            {"source": 1, "target": 7, "strip": false, "approx": true},
-            {"source": 1, "target": 8, "strip": false, "approx": true},
             {"source": 2, "target": 3, "strip": false, "approx": false},
-            {"source": 2, "target": 4, "strip": false, "approx": true},
-            {"source": 2, "target": 5, "strip": false, "approx": true},
-            {"source": 2, "target": 6, "strip": false, "approx": true},
-            {"source": 2, "target": 7, "strip": false, "approx": true},
-            {"source": 2, "target": 8, "strip": false, "approx": true},
             {"source": 3, "target": 4, "strip": false, "approx": false},
-            {"source": 3, "target": 5, "strip": false, "approx": true},
-            {"source": 3, "target": 6, "strip": false, "approx": true},
-            {"source": 3, "target": 7, "strip": false, "approx": true},
-            {"source": 3, "target": 8, "strip": false, "approx": true},
             {"source": 4, "target": 5, "strip": false, "approx": false},
-            {"source": 4, "target": 6, "strip": false, "approx": true},
-            {"source": 4, "target": 7, "strip": false, "approx": true},
-            {"source": 4, "target": 8, "strip": false, "approx": true},
             {"source": 5, "target": 6, "strip": false, "approx": false},
-            {"source": 5, "target": 7, "strip": false, "approx": true},
-            {"source": 5, "target": 8, "strip": false, "approx": true},
             {"source": 6, "target": 7, "strip": false, "approx": false},
-            {"source": 6, "target": 8, "strip": false, "approx": true},
             {"source": 7, "target": 8, "strip": false, "approx": false}
         ]
     };
 
-    let error = 20;
 
-// todo: dynamic height and width (so range should also be dynamic)
+    const error = 20;
+
     let height = 500;
     let width = 500;
     const xScale = d3.scaleLinear().domain([0, 100]).range([0, width]);
     const yScale = d3.scaleLinear().domain([100, 0]).range([0, height]);
 
-    let svg = d3.select("#vizDAG > svg")
+    let svg = d3.select("#vizDAG")
+        .append("svg")
         .attr("width", width)
         .attr("height", height)
-        .attr("style", "border: 1px solid black")
-        .append("g")
+        .attr("style", "border: 1px solid black");
+
+    let defs = svg.append("svg:defs");
+
+    defs.append("svg:marker")
+        .attr('id', 'arrow')
+        .attr('viewBox', '-0 -5 10 10')
+        .attr('refX', 13)
+        .attr('refY', 0)
+        .attr('orient', 'auto')
+        .attr('markerWidth', 5)
+        .attr('markerHeight', 5)
+        .attr('xoverflow', 'visible')
+        .append('svg:path')
+        .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
+        .attr('fill', '#000')
+        .style('stroke', 'none');
+
+    defs.append("svg:marker")
+        .attr('id', 'bluearrow')
+        .attr('viewBox', '-0 -5 10 10')
+        .attr('refX', 30)
+        .attr('refY', 0)
+        .attr('orient', 'auto')
+        .attr('markerWidth', 5)
+        .attr('markerHeight', 5)
+        .attr('xoverflow', 'visible')
+        .append('svg:path')
+        .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
+        .attr('fill', 'blue')
+        .style('stroke', 'none');
+
+    const vizContainer = svg.append("g")
         .attr("transform", function (d, i) {
             return "translate(0,0)";
         });
 
-    let stripContainer = svg
-        .append("g")
-        .attr("id", "strip-container");
+    const t = svg.transition()
+        .duration(750);
 
-    let chainContainer = svg.append("g")
-        .attr("id", "chain-container-1");
+    let stripContainer = vizContainer.append("g");
+    let edgeContainer = vizContainer.append("g");
+    let vertexContainer = vizContainer.append("g");
 
-    function drawGraph() {
-
-        let edges = chainContainer.selectAll("line")
-            .data(data.edges)
-            .enter()
-            .append("line")
-            .attr("x1", function (d) {
-                return xScale(data.nodes[d.source].coordinates.x);
-            })
-            .attr("y1", function (d) {
-                return yScale(data.nodes[d.source].coordinates.y);
-            })
-            .attr("x2", function (d) {
-                return xScale(data.nodes[d.target].coordinates.x);
-            })
-            .attr("y2", function (d) {
-                return yScale(data.nodes[d.target].coordinates.y);
-            })
-            .style("stroke", function (d) {
-                if (d.approx) {
-                    return "blue";
-                } else {
-                    return "#000000";
+    function generateApproxEdges() {
+        data.edges.forEach(function (e) {
+            e.approx = false;
+        });
+        const len = data.nodes.length;
+        data.nodes.forEach(function (n, idx) {
+            for (let i = 0; i < len; i++) {
+                if (i > idx) {
+                    data.edges.push({'source': idx, 'target': i, 'approx': true});
                 }
-            })
-            .style("stroke-dasharray", function (d) {
-                if (d.approx) {
-                    return "2,2";
-                } else {
-                    return "";
-                }
-            })
-            .style("stroke-width", function (d) {
-                if (d.approx) {
-                    return "1";
-                } else {
-                    return "2";
-                }
-            });
+            }
+        });
 
-        let vertices = chainContainer
+    }
+
+    generateApproxEdges();
+
+    function getEdges(showApprox) {
+        return data.edges.filter(function (e) {
+            if (showApprox) {
+                return true;
+            } else {
+                return !e.approx;
+            }
+
+        });
+    }
+
+    function drawGraph(showApprox) {
+
+        let edges = edgeContainer.selectAll("line")
+            .data(getEdges(showApprox), d => d)
+            .join(
+                enter => enter.append("line")
+                    .attr("x1", function (d) {
+                        return xScale(data.nodes[d.source].coordinates.x);
+                    })
+                    .attr("y1", function (d) {
+                        return yScale(data.nodes[d.source].coordinates.y);
+                    })
+                    .attr("x2", function (d) {
+                        return xScale(data.nodes[d.target].coordinates.x);
+                    })
+                    .attr("y2", function (d) {
+                        return yScale(data.nodes[d.target].coordinates.y);
+                    })
+                    .style("stroke", function (d) {
+                        if (d.approx) {
+                            return "blue";
+                        } else {
+                            return "#000000";
+                        }
+                    })
+                    .style("stroke-dasharray", function (d) {
+                        if (d.approx) {
+                            return "2,2";
+                        } else {
+                            return "";
+                        }
+                    })
+                    .style("stroke-width", function (d) {
+                        if (d.approx) {
+                            return "1";
+                        } else {
+                            return "2";
+                        }
+                    })
+                    .attr("marker-end", function (d) {
+                        if (d.approx) {
+                            return "url(#bluearrow)";
+                        } else {
+                            return "url(#arrow)";
+                        }
+                    })
+                    .call(enter => enter.transition(t)
+                        .attr("y", 0)),
+                update => update
+                    .attr("marker-end", "url(#arrow)")
+                    .attr("y", 0)
+                    .call(update => update.transition(t)
+                        .attr("x", (d, i) => i * 16)),
+                exit => exit
+                    .attr("fill", "brown")
+                    .call(exit => exit.transition(t)
+                        .attr("y", 30)
+                        .remove())
+            );
+
+
+        let vertices = vertexContainer
             .selectAll("circle")
             .data(data.nodes)
             .enter()
@@ -226,8 +279,11 @@ function drawDAG() {
     }
 
 
-    drawGraph();
     updateErrorDiscs();
+
+    return {
+        drawGraph: drawGraph
+    }
 }
 
 function drawParallelStrip() {
@@ -819,7 +875,21 @@ function drawDemo() {
 }
 
 
-drawDAG();
+let dag = drawDAG();
+let dagShowEdges = false;
+dag.drawGraph(dagShowEdges);
+
 drawParallelStrip();
 drawDiscsCones();
 drawDemo();
+
+function toggleDAGedges1() {
+    dagShowEdges = !dagShowEdges;
+    dag.drawGraph(dagShowEdges);
+}
+
+document.addEventListener('click', function (e) {
+    if (e.target && e.target.id === 'toggleDAGedges1') {
+        toggleDAGedges1();
+    }
+});
